f8dcfb33f4d1233bae964ceb9ebc159fe11a4cd4
Igor Zeljkovic <zeljkovic18@gmail.com>
2021-01-02 21:36:12 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# firefox-startpage
+
+Just a startpage for personal use where I test out some features/styles like startpage todo lists, weather API, links to most visited sites, backgrounds changing depending on time of the day, weather etc.
+Obviously most of this code will be awful meaning I'll probably have to refactor it some day cuz I'm embarrassed.
---
+++ weather.js
@@ -0,0 +1,37 @@
+(function() {
+    const WEATHERBIT_API_KEY = "0f031b64cc9d4a239f3f23d562456e91";
+    const BASE_URL = "http://api.weatherbit.io/v2.0/current";
+    const LAT_LONG = "?lat=45.267136&lon=19.833549"
+
+    var lastWeatherUpdate = localStorage.getItem('lastWeatherUpdate');
+    var weather = JSON.parse(localStorage.getItem('weather'));
+    
+    setWeatherDiv(weather.temp);
+
+    var d = new Date();
+    var timeNow = d.getHours();
+
+    if ((timeNow - lastWeatherUpdate) !== 0) {
+        console.log('Weather is updating...');
+        localStorage.setItem('lastWeatherUpdate', timeNow);
+
+        var url = BASE_URL + LAT_LONG + '&key=' + WEATHERBIT_API_KEY;
+
+        fetch(url)
+        .then(response => response.json())
+        .then(({data}) => {
+            setWeatherDiv(Math.floor(data[0].temp))
+
+            localStorage.setItem('weather', JSON.stringify(data[0]));
+        })
+    }
+
+    function setWeatherDiv(temperature) {
+        var weatherDiv = document.getElementsByClassName('weather')[0];
+        const degreeSymbol = "&#176";
+        
+        if (temperature !== null) {
+            weatherDiv.innerHTML = temperature + degreeSymbol + "C";
+        }
+    }
+})()
