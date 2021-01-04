8e25399c583f9d59081de009d0b5412e8c489899
Igor Zeljkovic <zeljkovic18@gmail.com>
2021-01-04 14:52:00 +0100

    Add a button that changes random backgrounds

--- startpage.js
+++ startpage.js
@@ -1,13 +1,15 @@
 (function() {
     setRandomBackground();
 
-    function setRandomBackground(theme = 'night') {
+    function setRandomBackground() {
+        var theme = 'night';
+
         var body = document.getElementsByTagName('body')[0];
         var randomBackgroundName = Math.floor(Math.random() * 35) + 1;
-        var productionUrl = '/mnt/Stuff/5_HOBBY/5_Programming/5_startpage';
-        var testUrl = '';
 
-        body.style.background = `url('${productionUrl}/backgrounds/${theme}/${randomBackgroundName}.gif')`;
+        body.style.background = `url('./backgrounds/${theme}/${randomBackgroundName}.gif')`;
         body.style.backgroundSize = 'cover';
     }
+
+    document.getElementById("changeBackground").onclick = setRandomBackground;
 })()
