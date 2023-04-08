8df51364ad9c26139c1dde4f652398897c12d802
Igor Zeljkovic <zeljkovic18@gmail.com>
2023-04-08 18:26:38 +0200

    Add more animated pixel wallpapers

--- startpage.js
+++ startpage.js
@@ -6,7 +6,7 @@ function setRandomBackground() {
     var theme = isDay() ? "day" : "night";
 
     var body = document.getElementsByTagName("body")[0];
-    var randomBackgroundName = Math.floor(Math.random() * 35) + 1;
+    var randomBackgroundName = Math.floor(Math.random() * 90) + 1;
 
     body.style.background = `url('./backgrounds/${theme}/${randomBackgroundName}.gif')`;
     body.style.backgroundSize = "cover";
